d2ae0f3213a01e782616d1cd40d991dd88a83515
agungalif <alifferizaagung@github.com>
2017-12-27 21:03:19 +0700

    dosen 0.2
    ini je

--- myweb/application/controllers/cdosen.php
+++ myweb/application/controllers/cdosen.php
@@ -13,7 +13,7 @@ class cdosen extends CI_Controller
 
 	public function index()
     {
-    	
+
         $datadosen['dosendata'] = json_decode($this->curl->simple_get($this->API.'/dosen'));
         $data=array(
 					'page_header' => "Data Dosen",
@@ -23,6 +23,85 @@ class cdosen extends CI_Controller
 		);
 		$this->parser->parse('starter',$data);
         // $this->load->view('dosen/form.php',$datadosen);
+    }
+
+    function create(){
+       
+          if(isset($_POST['simpan'])){
+            $data = array(
+            	'idDataDosen' =>$this->input->post('idDataDosen'),
+                'nidn'       =>  $this->input->post('nidn'),
+                'namaDosen'      =>  $this->input->post('namaDosen'),
+                );
+            $insert =  $this->curl->simple_post($this->API.'/dosen', $data, array(CURLOPT_BUFFERSIZE => 10)); 
+            if($insert)
+            {
+                $this->session->set_flashdata('hasil','Insert Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Insert Data Gagal');
+            }
+            redirect('index.php/cdosen');
+        }else{
+            $data['dosendata'] = json_decode($this->curl->simple_get($this->API.'/dosen'));
+         
+             $data=array(
+					'page_header' => "Data Dosen",
+					'page_desc' => "Daftar Dosen",
+					'menu_dosen' => 'active',
+					'page_content'=>$this->load->view('Dosen/edit',$data,TRUE),	
+		);
+		$this->parser->parse('starter',$data);
+        
+        }
+    }
+
+    function edit(){
+        if(isset($_POST['mit'])){
+            $data = array(
+            	'idDataDosen' =>$this->input->post('idDataDosen'),
+                'nidn'       =>  $this->input->post('nidn'),
+                'namaDosen'      =>  $this->input->post('namaDosen'),
+                );
+
+            $update =  $this->curl->simple_put($this->API.'/dosen', $data, array(CURLOPT_BUFFERSIZE => 10)); 
+            if($update)
+            {
+                $this->session->set_flashdata('hasil','Update Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Update Data Gagal');
+            }
+            redirect('index.php/cdosen');
+        }else{
+          	$params = array('idDataDosen'=>  $this->uri->segment(3));
+            $data['dosendata'] = json_decode($this->curl->simple_get($this->API.'/dosen',$params));
+            $data=array(
+					'page_header' => "Data Dosen",
+					'page_desc' => "Daftar Dosen",
+					'menu_dosen' => 'active',
+					'page_content'=>$this->load->view('Dosen/edit',$data,TRUE),	
+		);
+		$this->parser->parse('starter',$data);
+        }
+    }
+    
+    // delete data mahasiswa
+    function delete($idDataDosen){
+        if(empty($idDataDosen)){
+            redirect('cdosen');
+        }else{
+            $delete =  $this->curl->simple_delete($this->API.'/dosen', array('idDataDosen'=>$idDataDosen), array(CURLOPT_BUFFERSIZE => 10)); 
+            if($delete)
+            {
+                $this->session->set_flashdata('hasil','Delete Data Berhasil');
+            }else
+            {
+               $this->session->set_flashdata('hasil','Delete Data Gagal');
+            }
+            redirect('index.php/cdosen');
+
+        }
     }
 
 
---
+++ myweb/application/views/Dosen/edit.php
@@ -0,0 +1,66 @@
+<?php ; ?>
+  <div class="content">
+          <!-- Horizontal Form -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Form edit</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form action="<?php echo base_url('index.php/cdosen/edit') ?>" method="post" class="form-horizontal">
+              <div class="box-body">
+               <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">id</label>
+
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name="idDataDosen" placeholder="" value="<?php echo $dosendata[0]->idDataDosen; ?>" >
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">NIDN</label>
+
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name="nidn" placeholder="" value="<?php echo $dosendata[0]->nidn; ?>">
+                  
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Nama Dosen</label>
+
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" name="namaDosen" placeholder="" value="<?php echo $dosendata[0]->namaDosen; ?>">
+                  </div>
+                </div>
+                
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+              <a href="<?php echo base_url('index.php/cdosen/'); ?>" class="btn btn-default">kembali</a>
+               <button type="submit" class="btn btn-info pull-right" name="mit">simpan </button>
+
+                
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+          </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
