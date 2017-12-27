038a10a72472f56e7e61064ddd32f367ee691134
Mozaze Sanora Putra <mozazes42@gmail.com>
2017-12-27 16:11:31 +0700

    dosen
    upload

--- myserver/application/controllers/dosen.php
+++ myserver/application/controllers/dosen.php
@@ -33,15 +33,34 @@ class dosen extends REST_Controller
 		
 	}
 
-	function index_delete(){
-		$valueWhere=$this->delete('nidn');
-		$deleting=$this->model_akademik->delete('default','dosen','nidn',$valueWhere);
-		if ($deleting) {
-			$this->response(array('status'=>'seccess'),201);
+	function index_delete()
+	{
+		$db=$this->load->database('default',TRUE);
+		$key=$this->delete('idDataDosen');
+		$db->where('idDataDosen',$key);
+		$deleted=$db->delete('dosen');
+		if ($deleted) {
+			$this->response(array('status'=>'success'), 201);
 		} else {
-			$this->response(array('status'=>'fail'),201);
+			$this->response(array('status'=>'fail'),502);
 		}
-		
+	}
+
+	function index_put()
+	{
+		$db=$this->load->database('default',TRUE);
+		$key=$this->put('idDataDosen');
+		$data = array('idDataDosen' =>$this->put('idDataDosen') , 
+						'nidn' =>$this->put('nidn'),
+						'namaDosen'=>$this->put('namaDosen')
+		);
+		$db->where('idDataDosen',$key);
+		$updated=$db->update('dosen',$data);
+		if ($updated) {
+			$this->response(array('status'=>'success'),201);
+		} else {
+			$this->response(array('status'=>'fail'),501);
+		}
 	}
 }
 
